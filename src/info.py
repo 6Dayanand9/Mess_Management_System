f93cf2993e6797f8d90ec0b20ef713c7cebda303
Dayanand Nimbalkar <6nimbalkardayanand9@gmail.com>
2024-11-21 20:34:59 +0530

    info file added

---
+++ src/info.py
@@ -0,0 +1,37 @@
+class MemberInfo:
+    def __init__(self,id,nm,mealpd,fees_paid):
+        self.__id = id
+        self.__nm = nm
+        self.__fees_paid = fees_paid
+        self.__mealPerDay = mealpd
+    
+    def getid(self):
+        return self.__id
+    
+    def getName(self):
+        return self.__nm
+    
+    def getFeesPaid(self):
+        return self.__fees_paid
+
+    def getMeaLPerDay(self):
+        return self.__mealPerDay
+
+    def setId(self,id):
+        self.__id = id
+    
+    def setName(self,nm):
+        self.__nm = nm
+    
+    def setFeesPaid(self,fp):
+        self.__fees_paid = fp
+
+    def setMealPerDay(self,mp):
+        self.__mealPerDay = mp
+    
+    def __str__(self):
+        return "id :"+str(self.__id)+"\nname :"+self.__nm+"\nfees_paid :"+str(self.__fees_paid)+"\nmeal per day :"+str(self.__mealPerDay)
+#testing
+if(__name__)=="__main__":
+    I = MemberInfo(101,"daya",1,1000)
+    print(I)
