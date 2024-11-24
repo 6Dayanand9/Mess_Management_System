3b48fddd91e0c82a0a433240e5ae89f359798bb7
Dayanand Nimbalkar <6nimbalkardayanand9@gmail.com>
2024-11-25 01:03:31 +0530

    added the structure of search customer and delete customere method

--- src/mgmt.py
+++ src/mgmt.py
@@ -12,3 +12,42 @@ class Mgmt:
             #to display customer one by one
             for member in fp:
                 print(member)
+    
+    def searchEmp(self,id):
+        with open("data.txt",'r') as fp:
+            for e in fp:
+                try:
+                    e.index(str(id),4,7)
+                    print("record found")
+                    print(e)
+                    break
+                except ValueError:
+                    pass
+            else:
+                print("record Not found")
+
+
+    def DeleteEmp(self,id):
+        records = []
+        found = False
+        if len(str(id))==3:
+            with open("data.txt",'r') as fp:
+                for e in fp:
+                    try:
+                        e.index(str(id),4,7)
+                        #print(e)
+                    except ValueError:
+                        records.append(e)
+                    else:
+                        found = True
+
+            if(found == True):
+                with open("data.txt",'w') as fp:
+                    for e in records:
+                        fp.write(e)
+                print("id Deleted")
+            else:
+                print("record Not Found")
+        
+        else: 
+            print("Invalid ID")
