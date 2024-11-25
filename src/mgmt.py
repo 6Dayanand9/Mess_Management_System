e6ef484c38b2fece617806df09fd2ee2eb615ce8
Dayanand Nimbalkar <6nimbalkardayanand9@gmail.com>
2024-11-25 22:52:14 +0530

    created Update customer method to update the customer detail

--- src/mgmt.py
+++ src/mgmt.py
@@ -1,5 +1,6 @@
 import os
 class Mgmt:
+
     def addEmp(self,e):
         fp = open("data.txt",'a')
         fp.write(str(e))
@@ -15,14 +16,16 @@ class Mgmt:
     
     def searchEmp(self,id):
         with open("data.txt",'r') as fp:
+
             for e in fp:
                 try:
-                    e.index(str(id),4,7)
+                    e.index(str(id),0,4)
                     print("record found")
                     print(e)
                     break
                 except ValueError:
                     pass
+
             else:
                 print("record Not found")
 
@@ -30,14 +33,18 @@ class Mgmt:
     def DeleteEmp(self,id):
         records = []
         found = False
+
         if len(str(id))==3:
             with open("data.txt",'r') as fp:
+                
                 for e in fp:
                     try:
-                        e.index(str(id),4,7)
+                        e.index(str(id),0,4)
                         #print(e)
+
                     except ValueError:
                         records.append(e)
+
                     else:
                         found = True
 
@@ -46,8 +53,51 @@ class Mgmt:
                     for e in records:
                         fp.write(e)
                 print("id Deleted")
+
             else:
                 print("record Not Found")
         
         else: 
             print("Invalid ID")
+        
+
+    def UpdateCustomer(self,id):
+        myrecords = []
+        found=False
+        with open ("data.txt",'r') as fp:
+            for e in fp:
+                try:
+                    e.index(str(id),0,4)
+                
+                except:
+                    myrecords.append(e)
+                
+                else:
+                    ch = input("Do You want to change Name (y/n):")
+                    
+                    if ch.lower()=="y":
+                        name = input("Enter New Name")
+                        splitted_data=e.split(',')
+                        splitted_data[1]=name
+                        k= ','.join(splitted_data)
+                        myrecords.append(k)
+                    
+                    ch = input("Do You want to change fees_paid (y/n):")
+
+                    if ch.lower()=="y":
+                        fees = input("Enter fees paid")
+                        splitted_data=e.split()
+                        splitted_data[2]=fees
+                        k = ','.join(splitted_data)
+                        myrecords.append(k)
+                    
+                    found = True
+
+        if (found == True):
+            with open ("data.txt",'w') as fp:
+                for c in myrecords:
+                    fp.write(c)
+        
+        else:
+            print("record Not Found")
+
