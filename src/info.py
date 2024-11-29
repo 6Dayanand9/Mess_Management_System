1c8afe7bb2b0c214a66edd003f31aa57eb3b5e20
Dayanand Nimbalkar <6nimbalkardayanand9@gmail.com>
2024-11-29 19:54:00 +0530

    added new attributes such as phoneno,total_fees,fees_paid and remaining Fees

--- src/info.py
+++ src/info.py
@@ -1,9 +1,12 @@
 class MemberInfo:
-    def __init__(self,id,nm,mealpd,fees_paid):
+    def __init__(self,id,nm,mealpm,total_fees,fees_paid,fees_remaining,phoneno):
         self.__id = id
         self.__nm = nm
         self.__fees_paid = fees_paid
-        self.__mealPerDay = mealpd
+        self.__mealPerMonth = mealpm
+        self.__total_fees = total_fees
+        self.__remaining_fees = fees_remaining
+        self.__PhoneNo = phoneno
     
     def getid(self):
         return self.__id
@@ -13,9 +16,18 @@ class MemberInfo:
     
     def getFeesPaid(self):
         return self.__fees_paid
+    
+    def getPhoneno(self):
+        return self.__PhoneNo
 
-    def getMeaLPerDay(self):
-        return self.__mealPerDay
+    def getMeaLPerMonth(self):
+        return self.__mealPerMonth
+
+    def getTotalFees(self):
+        return self.__total_fees
+
+    def getRemainingFees(self):
+        return self.__remaining_fees
 
     def setId(self,id):
         self.__id = id
@@ -26,12 +38,21 @@ class MemberInfo:
     def setFeesPaid(self,fp):
         self.__fees_paid = fp
 
-    def setMealPerDay(self,mp):
-        self.__mealPerDay = mp
+    def setMealPerMonth(self,mp):
+        self.__mealPerMonth = mp
+    
+    def setTotalFees(self,tf):
+        self.__total_fees = tf
+    
+    def setRemainingFees(self,rf):
+        self.__remaining_fees =rf
+    
+    def SetPhoneno(self,ph):
+        self.__PhoneNo = ph
     
     def __str__(self):
-        return str(self.__id)+","+str(self.__nm)+","+str(self.__mealPerDay)+","+str(self.__fees_paid)+"\n"
+        return str(self.__id)+","+str(self.__nm)+","+str(self.__mealPerMonth)+","+str(self.__total_fees)+","+str(self.__fees_paid)+","+str(self.__remaining_fees)+","+str(self.__PhoneNo)+"\n"
 #testing
 if(__name__)=="__main__":
-    I = MemberInfo(101,"daya",1,1000)
+    I = MemberInfo(101,"daya",30,)
     print(I)
